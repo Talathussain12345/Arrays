46ce4ec8bb137eecd4146b753b426447c1f53327
Talathussain12345 <49521244+Talathussain12345@users.noreply.github.com>
2019-07-13 08:15:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,213 @@
+function firsrJs()
+{
+    alert("Congratulation ,Hello World");
+}
+// jsonlint
+
+let talatData =[
+{
+
+    "firstname" : "Talat",
+    "lastname" : "Hussain",
+    "age": 23 ,
+    "profaction" : "webdeveloper",
+    "nicNo" : "42401-234559877-9",
+    
+    "experience":"5years",
+    "onlinework/experience":"3years",
+    companyName:"Al-Karim Distributor",
+    onlineCompanyName:"Souq.com",
+
+    
+},
+
+{
+    "Qualification" : "Inter",
+    schoolName:"TcF",
+    collegeName:"national",
+    collegeAddress:"bahadrabad",
+    homeToCollegeDistance:"5km",
+
+}
+]
+
+let TalatWhatasAppData=[
+    {
+        name: "salman",
+        lastMsg: "hello",
+        cont: "23424567820"
+
+    },
+
+    {
+        name: "kashif",
+        lastMsg: "hi",
+        cont: "0987654321"
+    },
+    {
+        name: "salman",
+        lastMsg: "hy",
+        cont: "09878965432"
+    },
+
+
+
+        {
+            name: "kashif",
+            age: "23",
+            lastMsg: "hello",
+            cont: "23424567820"
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "salman",
+            age: "21",
+            lastMsg: "no",
+            cont: "2342456789920"
+
+        },
+        {
+            name: "rohan",
+            age: "56",
+            lastMsg: "hi",
+            cont: "2342456799820"
+
+        },
+        {
+            name: "tooba",
+            age: "21",
+            lastMsg: "hello",
+            cont: "2342456782990"
+
+        },
+        {
+            name: "kriti",
+            age: "18",
+            lastMsg: "yes",
+            cont: "234245678999920"
+
+        },
+        {
+            name: "kussar",
+            age: "20",
+            lastMsg: "how much?",
+            cont: "2342456780020"
+
+        },
+        {
+            name: "ramsha",
+            age: "20",
+            lastMsg: "okay",
+            cont: "2342456799820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "who?",
+            cont: "23424567820"
+
+        },
+        {
+            name: "RAFY",
+            age: "25",
+            lastMsg: "Theek ha",
+            cont: "2342456782000"
+
+        },
+        {
+            name: "MANSOOR",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "SOHAIL",
+            age: "20",
+            lastMsg: "No",
+            cont: "23424567820"
+
+        },
+        {
+            name: "ROHAL",
+            age: "26",
+            lastMsg: "NHI",
+            cont: "234249997820"
+
+        },
+        {
+            name: "FARHAN",
+            age: "21",
+            lastMsg: "OKAY",
+            cont: "2342456780887"
+
+        },
+        {
+            name: "raheel",
+            age: "29",
+            lastMsg: "kn",
+            cont: "234245687650",
+            companyName:"AVQ"
+
+
+
+        },
+        {
+            name: "SALAR",
+            age: "21",
+            lastMsg: "yes",
+            cont: "2342456798760",
+            lastMsgTime:"2:00pm"
+
+        },
+
+]
